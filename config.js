767beaf715870b2d8377ed9c209711578ea0b5e9
Jared Enck <jaredenck86@gmail.com>
2023-02-10 22:34:34 -0500

    updated config env variables

--- config.js
+++ config.js
@@ -3,30 +3,26 @@
 /** Shared config for application; can be required many places. */
 
 require("dotenv").config();
+const env = process.env
 require("colors");
 
-const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+const SECRET_KEY = env.SECRET_KEY || "secret-dev";
 
-const PORT = +process.env.PORT || 3001;
+const PORT = +env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+  const dbase = (env.NODE_ENV === "test")
+  ? env.DATABASE_TEST
+  : env.DATABASE;
+  const DB_URI =`socket:/var/run/postgresql?db=${dbase}`
+  return DB_URI
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
 //
 // WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
-const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
-
-console.log("Jobly Config:".green);
-console.log("SECRET_KEY:".yellow, SECRET_KEY);
-console.log("PORT:".yellow, PORT.toString());
-console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
-console.log("Database:".yellow, getDatabaseUri());
-console.log("---");
+const BCRYPT_WORK_FACTOR = env.NODE_ENV === "test" ? 1 : 12;
 
 module.exports = {
   SECRET_KEY,
--- config.test.js
+++ config.test.js
@@ -4,7 +4,7 @@ describe("config can come from env", function () {
   test("works", function() {
     process.env.SECRET_KEY = "abc";
     process.env.PORT = "5000";
-    process.env.DATABASE_URL = "other";
+    process.env.DATABASE = "other";
     process.env.NODE_ENV = "other";
 
     const config = require("./config");
@@ -16,7 +16,7 @@ describe("config can come from env", function () {
     delete process.env.SECRET_KEY;
     delete process.env.PORT;
     delete process.env.BCRYPT_WORK_FACTOR;
-    delete process.env.DATABASE_URL;
+    delete process.env.DATABASE;
 
     expect(config.getDatabaseUri()).toEqual("jobly");
     process.env.NODE_ENV = "test";
